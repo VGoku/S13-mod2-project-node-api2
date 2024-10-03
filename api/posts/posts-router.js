7ceebff3d32f569efbe45da4b8d8bdfbcfdaecf4
David S. <neojohn2312@gmail.com>
2024-10-03 15:29:55 -0500

    8th-commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -39,7 +39,26 @@ router.get("/:id", async (req, res) => {
     }
 })
 router.post("/", (req, res) => {
-
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post",
+        })
+    } else {
+        // console.log("success!!!")
+        Post.insert({ title, contents })
+        .then(stuff => {
+            console.log(stuff);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+        
+    }
 })
 router.delete("/:id", (req, res) => {
 
