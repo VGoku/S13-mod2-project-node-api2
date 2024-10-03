3daae6cdce711077ae25c0773470da062c06ea08
David S. <neojohn2312@gmail.com>
2024-10-03 14:11:10 -0500

    7th-commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -21,11 +21,18 @@ router.get("/", (req, res) => {
 router.get("/:id", async (req, res) => {
     try{
         // throw new Error("Very Bad Error!!")
-        const whatever = await Post.findById(req.params.id)
-        console.log("------->", whatever);
+        const post = await Post.findById(req.params.id)
+        // console.log("------->", whatever);
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            res.json(post)
+        }
     } catch (err) {
         res.status(500).json({
-            message: "The post with the specified ID does not exist",
+            message: "The post information could not be retrieved",
             err: err.message,
             stack: err.stack,
         })
