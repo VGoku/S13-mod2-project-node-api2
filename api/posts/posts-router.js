460b67028218ddebf3d768ca84a29cfffec0812e
David S. <neojohn2312@gmail.com>
2024-10-04 12:47:15 -0500

    10-coomit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -64,9 +64,51 @@ router.post("/", (req, res) => {
         
     }
 })
-router.delete("/:id", (req, res) => {
+// router.delete("/:id", async (req, res) => {
+//     try {
+//         // throw new Error("Sad......")
+//         const post = await Post.findById(req.params.id)
+//         if (!post) {
+//             res.status(404).json({
+//                 message: "The post with the specified ID does not exist",
+//             })
+//         } else {
+//             await Post.remove(req.params.id)
+//             res.json(post)
+//             console.log(numberOfDeletedPosts);
+//         }
+//     } catch (err) {
+//         res.status(500).json({
+//             message: "The post could not be removed",
+//             err: err.message,
+//             stack: err.stack,
+//         })
+//     }
+// })
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        console.log(`Attempting to delete post with ID: ${req.params.id}`);
 
-})
+        if (!post) {
+            return res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            });
+        } else {
+            await Post.remove(req.params.id);
+            console.log(`Deleted post: ${post}`);
+            res.json(post);
+        }
+    } catch (err) {
+        console.error(err); // Log the error for debugging
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack,
+        });
+    }
+});
+
 router.put("/:id", (req, res) => {
 
 })
